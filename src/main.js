97f78d634da3a64ecada8024ead157f9e521556e
bohdanD <doroshbohdan1@gmai.com>
2017-03-22 21:29:19 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MainContainer extends React.Component{
+    render(){
+        return (<h1>Hello</h1>);
+    }
+}
+
+
+ReactDOM.render(<MainContainer />, document.getElementById('root'));
