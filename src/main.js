3e7294f14a6505061d2a527ce6bac69cc0b63f07
bohdanD <doroshbohdan1@gmail.com>
2017-03-24 10:38:36 +0200

    changed main

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Container} from './container';
 
 class MainContainer extends React.Component{
     render(){
-        return (<h1>Hello dev branch</h1>);
-    }
-}
+        return <Container />
+    }   
+};
 
 
 ReactDOM.render(<MainContainer />, document.getElementById('root'));
