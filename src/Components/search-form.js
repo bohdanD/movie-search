bb53ceb46e64bf4b8327be4b29747125754a0c5e
bohdanD <doroshbohdan1@gmail.com>
2017-03-24 10:38:22 +0200

    Add Container and SearchForm

---
+++ src/Components/search-form.js
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+
+class SearchForm extends React.Component{
+
+    render(){
+        return <h1>SearchForm comp</h1>
+    }
+};
+
+export {SearchForm};
---
+++ src/container.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+import {SearchForm} from './Components/search-form';
+
+class Container extends React.Component{
+
+    render(){
+        return (<SearchForm />);
+    }
+};
+
+export {Container};
