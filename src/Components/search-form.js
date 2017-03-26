a000275122d30d0042aa9e665fe28d07b892f615
bohdanD <doroshbohdan1@gmail.com>
2017-03-26 13:04:10 +0300

    write search form

--- src/Components/search-form.js
+++ src/Components/search-form.js
@@ -3,9 +3,38 @@
 import React from 'react';
 
 class SearchForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {searchString: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({searchString: e.target.value});
+    }
+
+    handleSubmit(e){
+        console.log(this.state.searchString);
+        this.setState({searchString: ''});
+        e.preventDefault();
+    }
 
     render(){
-        return <h1>SearchForm comp</h1>
+        return (
+            <div className="serch-form">
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        name="searchInput" 
+                        placeholder="Input name here"
+                        onChange={this.handleChange}
+                    />
+                    <input type="submit" name="submitBtn" value="Search"/>
+                </form>
+            </div>
+        );
     }
 };
 
