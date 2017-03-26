12cc148ae4323cf964235cc0728516271c6b8959
bohdanD <doroshbohdan1@gmail.com>
2017-03-26 13:43:24 +0300

    add getXmlHttp func

---
+++ src/Constants/getXmlHttp.js
@@ -0,0 +1,21 @@
+
+
+//Source: http://xmlhttprequest.ru/
+function getXmlHttp(){
+  var xmlhttp;
+  try {
+    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (e) {
+    try {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (E) {
+      xmlhttp = false;
+    }
+  }
+  if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+    xmlhttp = new XMLHttpRequest();
+  }
+  return xmlhttp;
+}
+
+export default getXmlHttp;
