a68c83f54873de44492955acfaa264fd081584cc
bohdanD <doroshbohdan1@gmail.com>
2017-03-26 13:14:48 +0300

    add film and film list

---
+++ src/Components/film-list.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+import Film from './film';
+
+class FilmList extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return <Film />
+    }
+}
+
+export {FilmList};
---
+++ src/Components/film.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+class Film extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return <h3>Film component</h3>
+    }
+}
+
+export {Film};
