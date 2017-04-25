72d0ab89abaeda3aa3906463ca766bf163c85351
bohdanD <doroshbohdan1@gmail.com>
2017-04-25 11:02:54 +0300

    fixed bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
  
 module.exports = {
   entry: './src/main.js',
-  output: { path: __dirname, filename: 'bundle.js' },
+  output: { path: __dirname, filename: 'bundle.js', publicPath: __dirname },
   module: {
     loaders: [
       {
